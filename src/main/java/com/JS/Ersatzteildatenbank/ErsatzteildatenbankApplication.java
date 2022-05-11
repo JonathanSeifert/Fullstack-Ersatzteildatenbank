5516900b3471c8efc87fe57b86d044b376fd2337
Jonathan Seifert <jonathanseifert1998@gmail.com>
2022-05-11 14:38:15 +0200

    Spring Boot

---
+++ src/main/java/com/JS/Ersatzteildatenbank/ErsatzteildatenbankApplication.java
@@ -0,0 +1,13 @@
+package com.JS.Ersatzteildatenbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErsatzteildatenbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErsatzteildatenbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JS/Ersatzteildatenbank/ErsatzteildatenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JS.Ersatzteildatenbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErsatzteildatenbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
